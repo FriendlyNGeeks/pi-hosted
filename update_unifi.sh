d19a6fc2365e8785545f7e4f14bde5e44e2ac05d
FriendlyNGeeks <32318784+FriendlyNGeeks@users.noreply.github.com>
2022-05-12 21:53:51 -0400

    Update update_unifi.sh

--- update_unifi.sh
+++ update_unifi.sh
@@ -31,7 +31,7 @@ check_for_local_backup
 unifi_pid=`docker ps | grep unifi-rpi | awk '{print $1}'`
 unifi_name=`docker ps | grep unifi-rpi | awk '{print $2}'`
 
-echo "STEP 1 OF 4 | Attempting to STOP docker container by ID: $unifi_pid." | sudo docker stop $unifi_pid || error "Failed to stop unifi!"
-echo "STEP 2 OF 4 | Proceeding to REMOVE docker container by ID: $unifi_pid." | sudo docker rm $unifi_pid || error "Failed to remove unifi container!"
-echo "STEP 3 OF 4 | Commencing with UNTAGGING/REMOVING docker images by NAME: $unifi_name." | sudo docker rmi $unifi_name || error "Failed to remove/untag images from the container!"
-echo "STEP 4 OF 4 | Running docker up CLI." | sudo docker run -d --network host --name=unifi --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v config:/var/lib/unifi -v log:/usr/lib/unifi/logs -v log2:/var/log/unifi -v run:/usr/lib/unifi/run -v run2:/run/unifi -v work:/usr/lib/unifi/work ryansch/unifi-rpi:latest || error "Failed to execute newer version of Portainer!"
+echo "STEP 1 OF 4 | Attempting to STOP docker container by ID: $unifi_pid." && sudo docker stop $unifi_pid || error "Failed to stop unifi!"
+echo "STEP 2 OF 4 | Proceeding to REMOVE docker container by ID: $unifi_pid." && sudo docker rm $unifi_pid || error "Failed to remove unifi container!"
+echo "STEP 3 OF 4 | Commencing with UNTAGGING/REMOVING docker images by NAME: $unifi_name." && sudo docker rmi $unifi_name || error "Failed to remove/untag images from the container!"
+echo "STEP 4 OF 4 | Running docker up CLI." && sudo docker run -d --network host --name=unifi --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v config:/var/lib/unifi -v log:/usr/lib/unifi/logs -v log2:/var/log/unifi -v run:/usr/lib/unifi/run -v run2:/run/unifi -v work:/usr/lib/unifi/work ryansch/unifi-rpi:latest || error "Failed to execute newer version of Portainer!"
