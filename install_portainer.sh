892328a7b8de68b4fcbecfb84d565a40776ef848
FriendlyNGeeks <32318784+FriendlyNGeeks@users.noreply.github.com>
2026-02-17 18:28:41 -0500

    Fix formatting issues in install_portainer.sh

--- install_portainer.sh
+++ install_portainer.sh
@@ -29,6 +29,6 @@ function check_internet() {
 
 check_internet
 
-$SUDO docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
-$SUDO docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest --logo "https://pi-hosted.com/pi-hosted-logo.png" --templates "https://raw.githubusercontent.com/pi-hosted/pi-hosted/master/template/portainer-v2-arm64.json" || error "Failed to run Portainer docker image!"
+SUDO docker pull portainer/portainer-ce:latest || error "Failed to pull latest Portainer docker image!"
+SUDO docker run -d -p 9000:9000 -p 9443:9443 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce:latest --logo "https://pi-hosted.com/pi-hosted-logo.png" --templates "https://raw.githubusercontent.com/pi-hosted/pi-hosted/master/template/portainer-v2-arm64.json" || error "Failed to run Portainer docker image!"
 
